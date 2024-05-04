b9b199c3e0a820fe25c1f9ed02212c1f556a1936
sarace <sarace@huisa.win>
2024-05-04 17:07:14 +0800

    add clang-format and pre-commit

--- main.cxx
+++ main.cxx
@@ -11,6 +11,8 @@
  */
 
 #include <algorithm>
+#include <array>
+#include <cmath>
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
@@ -42,7 +44,7 @@
  * @param mode file mode
  * @return std::string terminal color control string
  */
-std::string colorOfMode(const std::string &mode) {
+auto colorOfMode(const std::string &mode) -> std::string {
   if (mode[0] == 'd') {
     return "\033[1;34m";
   }
@@ -52,9 +54,9 @@ std::string colorOfMode(const std::string &mode) {
   return "\033[0m";
 }
 
-bool isPathExist(const std::string &path) {
-  struct stat st;
-  return (stat(path.c_str(), &st) == 0);
+auto isPathExist(const std::string &path) -> bool {
+  struct stat pathStat;
+  return (stat(path.c_str(), &pathStat) == 0);
 }
 
 /**
@@ -63,18 +65,22 @@ bool isPathExist(const std::string &path) {
  * @param size file size
  * @return std::string human readable string of file size
  */
-std::string convertSize2HumanReadable(off_t size) {
+auto convertSize2HumanReadable(off_t size) -> std::string {
   std::stringstream result;
-  if (size < 1024) {
+  const std::uint64_t constexpr KB_BORDER = 1024LL;
+  const std::uint64_t constexpr MB_BORDER = KB_BORDER * KB_BORDER;
+  const std::uint64_t constexpr GB_BORDER = MB_BORDER * KB_BORDER;
+  const std::double_t constexpr DIV_MAGIC = 1024.0;
+  if (size < KB_BORDER) {
     result << size << 'B';
-  } else if (size > 1024LL && size < 1024LL * 1024) {
-    double num = size / 1024.0;
+  } else if (size > KB_BORDER && size < MB_BORDER) {
+    double num = size / DIV_MAGIC;
     result << std::fixed << std::setprecision(1) << num << 'K';
-  } else if (size > 1024LL * 1024 && size < 1024LL * 1024 * 1024) {
-    double num = static_cast<double>(size) / (1024 * 1024);
+  } else if (size > MB_BORDER && size < GB_BORDER) {
+    double num = static_cast<double>(size) / (MB_BORDER);
     result << std::fixed << std::setprecision(1) << num << 'M';
-  } else if (size > 1024LL * 1024 * 1024) {
-    double num = static_cast<double>(size) / (1024LL * 1024 * 1024);
+  } else if (size > GB_BORDER) {
+    double num = static_cast<double>(size) / (GB_BORDER);
     result << std::fixed << std::setprecision(1) << num << 'G';
   }
   return result.str();
@@ -86,11 +92,11 @@ std::string convertSize2HumanReadable(off_t size) {
  * @param time unix timestamp
  * @return std::string time string
  */
-std::string getTimeStrOfUnixTime(long time) {
+auto getTimeStrOfUnixTime(long time) -> std::string {
   std::time_t time_tmp = time;
-  std::stringstream ss;
-  ss << std::put_time(std::localtime(&time_tmp), "%b %d %H:%M");
-  return ss.str();
+  std::stringstream stringstream;
+  stringstream << std::put_time(std::localtime(&time_tmp), "%b %d %H:%M");
+  return stringstream.str();
 }
 
 /**
@@ -99,53 +105,59 @@ std::string getTimeStrOfUnixTime(long time) {
  * @param path path to list
  */
 void doListDir(const std::string &path) {
-  DIR *dp = opendir(path.c_str());
-  if (dp == nullptr) {
+  DIR *dirPtr = opendir(path.c_str());
+  if (dirPtr == nullptr) {
     std::cerr << "Could not open directory " << path << std::endl;
     exit(-2);
   }
   struct dirent *ent = nullptr;
   std::vector<std::pair<long, std::string>> container;
 
-  while ((ent = readdir(dp)) != nullptr) {
-    std::string full_path = path + PATH_SEP + ent->d_name;
-    struct stat st;
-    if (stat(full_path.c_str(), &st) == 0) {
-      char s[256];
+  while ((ent = readdir(dirPtr)) != nullptr) {
+    std::string fullPath = path + PATH_SEP + ent->d_name;
+    struct stat fullPathStat;
+    if (stat(fullPath.c_str(), &fullPathStat) == 0) {
+      const std::int32_t constexpr MODE_LEN = 12;
+      const std::int32_t constexpr EXTRA_POS = 10;
+      std::array<char, MODE_LEN> modeStr;
       // get str mode
-      strmode(st.st_mode, s);
+      strmode(fullPathStat.st_mode, modeStr.data());
       // get extended attributes
-      int buf_len = listxattr(full_path.c_str(), nullptr, 0, XATTR_NOFOLLOW);
+      int buf_len = listxattr(fullPath.c_str(), nullptr, 0, XATTR_NOFOLLOW);
       if (buf_len > 0) {
-        s[10] = '@';
+        modeStr[EXTRA_POS] = '@';
       }
       // get acl flags
-      acl_t acl = acl_get_file(full_path.c_str(), ACL_TYPE_EXTENDED);
+      acl_t acl = acl_get_file(fullPath.c_str(), ACL_TYPE_EXTENDED);
       if (acl != nullptr) {
-        s[10] = '+';
+        modeStr[EXTRA_POS] = '+';
       }
       // get user name
-      struct passwd *pw = getpwuid(st.st_uid);
-      if (pw == nullptr) {
+      struct passwd *passwdStruct = getpwuid(fullPathStat.st_uid);
+      if (passwdStruct == nullptr) {
         std::cerr << "Couldn't get user name" << std::endl;
         return;
       }
       // get group name
-      struct group *gr = getgrgid(st.st_gid);
-      if (gr == nullptr) {
+      struct group *groupStruct = getgrgid(fullPathStat.st_gid);
+      if (groupStruct == nullptr) {
         std::cerr << "Couldn't get group name" << std::endl;
         return;
       }
       // get file size
-      std::string size = convertSize2HumanReadable(st.st_size);
+      std::string size = convertSize2HumanReadable(fullPathStat.st_size);
       // get date string
-      std::string time = getTimeStrOfUnixTime(st.st_mtimespec.tv_sec);
-
-      std::stringstream ss;
-      ss << s << "  " << std::setw(2) << st.st_nlink << " " << pw->pw_name
-         << "  " << gr->gr_name << " " << std::setw(6) << size << " " << time
-         << " " << colorOfMode(s) << ent->d_name << "\033[0m" << std::endl;
-      container.emplace_back(st.st_mtimespec.tv_sec, ss.str());
+      std::string time = getTimeStrOfUnixTime(fullPathStat.st_mtimespec.tv_sec);
+      const std::uint32_t constexpr SIZE_WIDTH = 6;
+      std::stringstream stringstream;
+      stringstream << modeStr.data() << "  " << std::setw(2)
+                   << fullPathStat.st_nlink << " " << passwdStruct->pw_name
+                   << "  " << groupStruct->gr_name << " "
+                   << std::setw(SIZE_WIDTH) << size << " " << time << " "
+                   << colorOfMode(modeStr.data()) << ent->d_name << "\033[0m"
+                   << std::endl;
+      container.emplace_back(fullPathStat.st_mtimespec.tv_sec,
+                             stringstream.str());
     }
   }
   // sort by last modify time
@@ -165,13 +177,13 @@ void listDir(const std::string &path) {
   doListDir(path);
 }
 
-int main(int argc, char *argv[]) {
+auto main(int argc, char *argv[]) -> int {
   if (argc != 2) {
     std::cerr << "Usage: " << argv[0] << "<dir>" << std::endl;
     return 1;
   }
   char *path = argv[1];
-  if (path != NULL) {
+  if (path != nullptr) {
     listDir(path);
   }
   return 0;
